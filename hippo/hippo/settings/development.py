66f404a7e8800570d006cb339f778dc55f5e8aaf
Sardor Muminov <sardor@insilicogen.com>
2016-09-08 23:11:23 +0900

    Access keys

--- hippo/hippo/settings/development.py
+++ hippo/hippo/settings/development.py
@@ -1,4 +1,7 @@
-from hippo.settings import *
+import os
+from hippo.settings import *  # noqa
 
 DEBUG = True
-INSTALLED_APPS.append('debug_toolbar')
+INSTALLED_APPS.append('debug_toolbar')  # noqa
+BACKEND_ACCESS_KEY = os.getenv('ACCESS_KEY')
+BACKEND_SECRET_KEY = os.getenv('SECRET_KEY')
