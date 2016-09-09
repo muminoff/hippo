1f030f95a1c72ff0fc012caef38310d592c1a368
Sardor Muminov <sardor@insilicogen.com>
2016-09-09 09:54:31 +0900

    Comment

--- hippo/test_buckets.py
+++ hippo/test_buckets.py
@@ -9,9 +9,11 @@ def main():
         's3',
         endpoint_url='http://192.168.99.100:8080')
 
-    response = s3client.list_objects(Bucket='d826d38d2c7442dcb6ddd49f43491e7e')
-    print(response['Contents'])
+    # response = s3client.list_objects(Bucket='d826d38d2c7442dcb6ddd49f43491e7e')
+    # print(response['Contents'])
 
+    # response = s3client.get_bucket_location(Bucket='d826d38d2c7442dcb6ddd49f43491e7e')
+    # print(response)
 
 if __name__ == '__main__':
     main()
