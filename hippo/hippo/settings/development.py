f8653a9c933b3aef17a37356bd41b6c2407e55c0
Sardor Muminov <sardor@insilicogen.com>
2016-09-09 09:01:27 +0900

    URL fixed

--- hippo/hippo/settings/development.py
+++ hippo/hippo/settings/development.py
@@ -3,6 +3,6 @@ from hippo.settings import *  # noqa
 
 DEBUG = True
 INSTALLED_APPS.append('debug_toolbar')  # noqa
-BACKEND_ENDPOINT_URL = 'http://192.168.99.100:8080'
+BACKEND_ENDPOINT_URL = os.getenv('BACKEND_ENDPOINT_URL', 'http://192.168.99.100:8080')  # noqa
 BACKEND_ACCESS_KEY = os.getenv('ACCESS_KEY')
 BACKEND_SECRET_KEY = os.getenv('SECRET_KEY')
